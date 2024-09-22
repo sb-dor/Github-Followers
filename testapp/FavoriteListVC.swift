977e438c82c9f2237a91cdb5577e5eba7f84ff30
sb-dor <avaz.shams.2002@gmail.com>
2024-09-22 15:45:16 +0500

    added navigation controllers to  navigate between screens

---
+++ testapp/FavoriteListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteListVC.swift
+//  testapp
+//
+//  Created by Avaz on 22/09/24.
+//
+
+import UIKit
+
+class FavoriteListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ testapp/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  testapp
+//
+//  Created by Avaz on 22/09/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
