6a3e794bf56717eb2f53620b96a30941359ecb79
sb-dor <avaz.shams.2002@gmail.com>
2024-09-24 13:53:59 +0500

    added some info about classes and structors

--- testapp/Managers/NetworkManager.swift
+++ testapp/Managers/NetworkManager.swift
@@ -7,6 +7,21 @@
 
 import Foundation
 
+
+//  Class :
+//
+//   - Reference Type: Imagine a class like a shared document. When many people open the same document, they're all looking at the same thing. If someone makes changes, everyone sees those changes because they're looking at the same paper.
+
+//   - Inheritance: Classes can have children, like a family tree. A child can inherit traits from their parents. For example, a "Car" class can have a child class like "SportsCar" that inherits features from the parent class.
+
+//   - Mutable: You can change a class after you create it. It's like being able to erase and rewrite on a piece of paper.
+//
+//  Structs :
+//
+//   - Value Type: Think of a struct like a personal notebook. When you copy it, you get your own new notebook. If you make notes in your notebook, it doesn't affect someone else's notebook because they each have their own separate pages.
+//   - No Inheritance: Structs can't have children or parents. Each struct is on its own; they can't inherit anything from other structs.
+//   - Immutable: Once you create a struct, you can't change it. It's like having a notebook with pages that can't be erased or rewritten.
+
 class NetworkManager {
     static let shared = NetworkManager()
     let baseUrl = "https://api.github.com/users/"
--- testapp/Models/Follower.swift
+++ testapp/Models/Follower.swift
@@ -8,6 +8,23 @@
 import Foundation
 
 
+//  Class :
+//
+//   - Reference Type: Imagine a class like a shared document. When many people open the same document, they're all looking at the same thing. If someone makes changes, everyone sees those changes because they're looking at the same paper.
+
+//   - Inheritance: Classes can have children, like a family tree. A child can inherit traits from their parents. For example, a "Car" class can have a child class like "SportsCar" that inherits features from the parent class.
+
+//   - Mutable: You can change a class after you create it. It's like being able to erase and rewrite on a piece of paper.
+//
+//  Structs :
+//
+//   - Value Type: Think of a struct like a personal notebook. When you copy it, you get your own new notebook. If you make notes in your notebook, it doesn't affect someone else's notebook because they each have their own separate pages.
+//   - No Inheritance: Structs can't have children or parents. Each struct is on its own; they can't inherit anything from other structs.
+//   - Immutable: Once you create a struct, you can't change it. It's like having a notebook with pages that can't be erased or rewritten.
+
+
+
+
 // Codable is the thing that when you write fields for it
 // it should be exactly the same as json field
 // it will match that and autmotaically set the value into field
