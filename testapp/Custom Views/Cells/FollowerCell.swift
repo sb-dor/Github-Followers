750eef38bcafeecf25518896e81d894cf22e4426
sb-dor <avaz.shams.2002@gmail.com>
2024-09-24 21:50:12 +0500

    added Follower Cell vc

---
+++ testapp/Custom Views/Cells/FollowerCell.swift
@@ -0,0 +1,53 @@
+//
+//  FollowerCell.swift
+//  testapp
+//
+//  Created by Avaz on 24/09/24.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+    static let reuseId = "FollowerCell"
+    let avatartImageView = GFAvatarImageView(frame: .zero)
+    let userNameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    let padding : CGFloat = 8
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureAvatartImageVoew()
+        configureUserNameLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower?) {
+        userNameLabel.text = follower?.login
+        
+    }
+    
+    private func configureAvatartImageVoew() {
+        addSubview(avatartImageView)
+    
+        NSLayoutConstraint.activate([
+            avatartImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatartImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatartImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatartImageView.heightAnchor.constraint(equalTo: avatartImageView.widthAnchor)
+        ])
+    }
+    
+    private func configureUserNameLabel() {
+        addSubview(userNameLabel)
+        
+        NSLayoutConstraint.activate([
+            userNameLabel.topAnchor.constraint(equalTo: avatartImageView.bottomAnchor, constant: 20),
+            userNameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            userNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            userNameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
---
+++ testapp/Custom Views/GFAvatarImageCiew.swift
@@ -0,0 +1,20 @@
+//
+//  GFAvatarImageCiew.swift
+//  testapp
+//
+//  Created by Avaz on 24/09/24.
+//
+
+import UIKit
+
+class GFAvatarImageCiew: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
